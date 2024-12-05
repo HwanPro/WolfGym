d1162b222b5d0f149a1dc301a753ae193f5a9d21
HwanPro <168.1.2.0.20a@gmail.com>
2024-12-05 00:11:11 -0500

    cambios en depencias y codigo

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,22 +1,26 @@
-// types/next-auth.d.ts
-
-import NextAuth, { DefaultSession } from "next-auth";
-import "next-auth/jwt";
+// src/types/next-auth.d.ts
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      role?: string;
-      emailVerified?: Date | null;
+      role: string;
+      emailVerified: boolean;
     } & DefaultSession["user"];
+  }
+
+  interface User extends DefaultUser {
+    id: string;
+    role: string;
+    emailVerified: boolean;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     id: string;
-    role?: string;
-    emailVerified?: Date | null;
+    role: string;
+    emailVerified: boolean;
   }
 }

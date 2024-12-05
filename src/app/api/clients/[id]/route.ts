67f20c480c00dd7f0be594ef07409d71a0a176ad
HwanPro <168.1.2.0.20a@gmail.com>
2024-12-05 01:30:23 -0500

    Route id

--- src/app/api/clients/[id]/route.ts
+++ src/app/api/clients/[id]/route.ts
@@ -20,9 +20,9 @@ const clientUpdateSchema = z.object({
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: Record<string, string> }
+  context: { params: { id: string } }
 ) {
-  const id = parseInt(params.id, 10);
+  const id = parseInt(context.params.id, 10);
 
   if (isNaN(id)) {
     return NextResponse.json(
@@ -58,9 +58,9 @@ export async function GET(
 
 export async function PUT(
   req: NextRequest,
-  { params }: { params: Record<string, string> }
+  context: { params: { id: string } }
 ) {
-  const id = parseInt(params.id, 10);
+  const id = parseInt(context.params.id, 10);
 
   if (isNaN(id)) {
     return NextResponse.json(
@@ -122,9 +122,9 @@ export async function PUT(
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: Record<string, string> }
+  context: { params: { id: string } }
 ) {
-  const id = parseInt(params.id, 10);
+  const id = parseInt(context.params.id, 10);
 
   if (isNaN(id)) {
     return NextResponse.json(
